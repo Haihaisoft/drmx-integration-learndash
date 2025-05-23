303d08edaf79347ac3a1ff35a9af3f1e6dcf069e
Haihaisoft <126839650+Haihaisoft@users.noreply.github.com>
2025-05-23 17:54:09 +0800

    Update drmx_login.php

--- drmx_login.php
+++ drmx_login.php
@@ -1,11 +1,24 @@
 <?php
-session_start();
-require '../../../wp-load.php';
-error_reporting(E_ALL & ~E_DEPRECATED);
+require_once dirname(__DIR__, 3) . '/wp-load.php';
+error_reporting(E_ALL & ~E_DEPRECATED); 
+
+$userid = get_current_user_id();
+
+$user 		= get_user_by( 'id', $userid );
+$username 	= $user->user_login;
+$userEmail 	= $user->user_email;
+
+if (!session_id()) {
+    session_start();
+}
+
+$cache_key = isset($_GET['cache_key']) ? sanitize_text_field($_GET['cache_key']) : 'drmx_temp_params_' . session_id();
+wp_cache_delete($cache_key, 'options');
+$drmx_params = get_option($cache_key);
 
 $flag	= 0;
 // Get the yourproductid set in the license Profile.
-// $PIDs	= explode('-', $_SESSION['ProductID']);
+$PIDs	= explode('-', $drmx_params["yourproductid"]);
 
 // Get integration parameters
 define( 'DRMX_ACCOUNT', 		get_option('drmx_account'));
@@ -17,125 +30,110 @@ define( 'DRMX_BINDING', 		get_option('drmx_binding'));
 
 $client = new SoapClient(WSDL, array('trace' => false));
 
-if($_POST){
-	$username 	= $_REQUEST["username"];
-	$pwd 		= $_REQUEST["password"];
-	$user		= get_user_by('login', $username); 
-	$userid 	= $user->ID;
-	$user_pass 	= $user->user_pass;
-	$userEmail	= $user->user_email;
-
-	if (!wp_check_password($pwd, $user_pass)) {
-		echo "<SCRIPT language=JavaScript>location='drmx_LicError.php?message=Your Account or Password is incorrect! <br><br>Please revisit the course!<br>&error=null'</SCRIPT>";
-		exit;
-	}
-}else{
-	$userid 	= get_current_user_id();
-	$user 		= get_user_by( 'id', $userid );
-	$username 	= $user->user_login;
-	$userEmail 	= $user->user_email;
-}
 
 /********* Business logic verification ***********/
 
-/****** Connect database configuration *******/
-$dbcon=mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-if ($dbcon -> connect_errno) {
-	echo "Failed to connect to MySQL: " . $dbcon -> connect_error;
-	exit();
-}
-mysqli_set_charset ($dbcon,'utf8');
-
-// Get website db table prefix
-$table_prefix = $wpdb->prefix;
-
-// Get Course Access Expiration
-$courseResult = mysqli_query($dbcon,"SELECT meta_value FROM ".$table_prefix."postmeta WHERE meta_key='_sfwd-courses' AND post_id='".$_SESSION['ProductID']."'");
-$courseRow = mysqli_fetch_array($courseResult, MYSQLI_ASSOC);
-$sfwd_courses_expire_access = strstr($courseRow['meta_value'], '"sfwd-courses_expire_access";s:2:"on"');
-
-// Course Access Expiration   
-if($sfwd_courses_expire_access != FALSE){
-	$newStr = substr(strstr($courseRow['meta_value'], '"sfwd-courses_expire_access_days";i:'), 36);
-	$rights_duration = substr($newStr,0,strpos($newStr,';')); //Access Period 
-}else{
-	$rights_duration = "-1"; //Access Period 
-}
-
-// user user group course 
-$meta_key = "course_".$_SESSION['ProductID']."_access_from";
-
-$result = mysqli_query($dbcon,"SELECT meta_value FROM ".$table_prefix."usermeta WHERE meta_key='".$meta_key."' AND user_id='".$userid."'");
-$resultRow = mysqli_fetch_array($result, MYSQLI_ASSOC);
-$isEnrolled = $resultRow['meta_value'];
-
-if($isEnrolled == NULL){
-	// Get the user group set by the course
-	$course_group_result = mysqli_query($dbcon,"SELECT meta_key FROM ".$table_prefix."postmeta WHERE meta_key LIKE 'learndash_group_enrolled_%' AND post_id='".$_SESSION['ProductID']."'");
-	$course_group_resultRow = mysqli_fetch_all($course_group_result, MYSQLI_ASSOC);
+foreach ($PIDs as $Pid) {
+	if (!isset($Pid) || empty($Pid)) {
+		$info = "Invalid Course ID";
+	} else {
+		// Get course ID
+		$course_id = $Pid;
 	
-	//Find out if a user belongs to this user group
-	foreach($course_group_resultRow as $group){
-		$group_id = str_replace('learndash_group_enrolled_','',$group['meta_key']);
+		$isEnrolled = is_user_enrolled_in_course_alt($userid, $course_id);
 
-		$user_group_result = mysqli_query($dbcon,"SELECT meta_value FROM ".$table_prefix."usermeta WHERE meta_key='learndash_group_users_".$group_id."' AND user_id='".$userid."'");
-		$user_group_resultRow = mysqli_fetch_array($user_group_result, MYSQLI_ASSOC);
-		$isEnrolled = $user_group_resultRow['meta_value'];
-		if($isEnrolled != NULL){
-			break;
+		// Get course access expiration settings
+		$course_details = get_course_access_details($course_id);
+		// if Course Access Expiration is false, set expire_days to -1
+		if ($course_details['expire_enabled']) {
+			$rights_expire_days = $course_details['expire_days'];
+		} else {
+			$rights_expire_days = "-1";
+		}
+		// Get start and end date
+		$rights_start_date = date('Y/m/d', $course_details['start_date']);
+		$rights_end_date = date('Y/m/d', $course_details['end_date']);
+		// Set the Rights duration
+		$rights_duration = [
+			'BeginDate' => $rights_start_date,
+			'ExpirationDate' => $rights_end_date,
+			'ExpirationAfterFirstUse' => $rights_expire_days,
+		];
+
+		if($isEnrolled){
+			// If the username is not exists, call 'AddNewUser' to add user.
+			if(checkUserExists($client, $username) == "False"){
+				$addNewUserResult = addNewUser($client, $username, $userEmail);
+				$info = $addNewUserResult;
+			}
+			// check user is revoked
+			/*if(checkUserIsRevoked($client, $username)){
+				$errorInfo = 'Username: '.$username.' is revoked.';
+				header("location:drmx_LicError.php?error=".$errorInfo."&message=".$message);
+				exit;
+			}*/
+	
+			/*** Automatically update license permissions for users based on duration of the course ****/
+			$updateRightResult = updateRight($client, $rights_duration, $userEmail);
+	
+			if($updateRightResult == '1'){
+				
+				/*****After the License Rights is updated, perform the method of obtaining the license****/
+				$licenseResult = getLicense($client, $username);
+				$license = $licenseResult->getLicenseRemoteToTableWithVersionWithMacResult;
+				$message = $licenseResult->Message;
+	
+				if(stripos($license, '<div id="License_table_DRM-x4" style="display:none;">' )  === false ){
+					header('location: drmx_LicError.php?error='.$license.'&message='.$message);
+					exit;
+				}
+				/*****After obtaining the license, store the license and message through the session, and then direct to the licstore page******/
+				$license_params = [
+					'license'    	=> isset($license) ? $license : '',
+					'message'   	=> isset($message) ? $message : '',
+					'return_url'   	=> $drmx_params["return_url"],
+				];
+				$license_cache_key = 'license_temp_params_' . session_id();
+				wp_cache_delete($license_cache_key, 'options');
+				update_option($license_cache_key, $license_params, false);
+				
+				$flag = 1;
+				header('location: licstore.php');
+				$info = "Getting license...";
+				exit;
+			}
 		}
 	}
 }
 
-if($isEnrolled != NULL){
-	// If the username is not exists, call 'AddNewUser' to add user.
-	if(checkUserExists($client, $username) == "False"){
-		$addNewUserResult = addNewUser($client, $username, $userEmail);
-		$info = $addNewUserResult;
-		$flag = 1;
-	}
-	// check user is revoked
-	/*if(checkUserIsRevoked($client, $username)){
-		$errorInfo = 'Username: '.$username.' is revoked.';
-		header("location:drmx_LicError.php?error=".$errorInfo."&message=".$message);
-		exit;
-	}*/
-
-	/*** Automatically update license permissions for users based on duration of the course ****/
-	$updateRightResult = updateRight($client, $rights_duration, $userEmail);
-
-	if($updateRightResult == '1'){
-		
-		/*****After the License Rights is updated, perform the method of obtaining the license****/
-		$licenseResult = getLicense($client, $username);
-		$license = $licenseResult->getLicenseRemoteToTableWithVersionResult;
-		$message = $licenseResult->Message;
-
-		if(stripos($license, '<div id="License_table_DRM-x4" style="display:none;">' )  === false ){
-			header('location: drmx_LicError.php?error='.$license.'&message='.$message);
-			exit;
-		}
-		/*****After obtaining the license, store the license and message through the session, and then direct to the licstore page******/
-		$_SESSION['license'] = $license;
-		$_SESSION['message'] = $message;
-		
-		header('location: licstore.php');
-		$flag = 1;
-		$info = "Getting license...";
-		exit;
-	}else{
-		$info = $updateRightResult;
-		$flag = 1;
-	}
+if($flag == 0){
+	$info = "You have not enrolled for this course.";
 }
-
-if($flag == 0) {
-	$info = "You have not enrolled the course.";
-}
-
-mysqli_close($dbcon);
 
 /***** End of business logic verification ******/
+// Check if the user is enrolled in the course
+function is_user_enrolled_in_course_alt($userid, $course_id) {
+    $enrolled_courses = learndash_user_get_enrolled_courses($userid);
+    return in_array($course_id, $enrolled_courses);
+}
+
+// Get course access expiration settings
+function get_course_access_details($course_id) {
+    $raw = get_post_meta($course_id, '_sfwd-courses', true);
+	$data = maybe_unserialize($raw);
+
+	$start_timestamp = isset($data['sfwd-courses_course_start_date']) ? intval($data['sfwd-courses_course_start_date']) : null;
+	$end_timestamp = isset($data['sfwd-courses_course_end_date']) ? intval($data['sfwd-courses_course_end_date']) : null;
+	$expire_enabled = isset($data['sfwd-courses_expire_access']) && $data['sfwd-courses_expire_access'] === 'on';
+	$expire_days = isset($data['sfwd-courses_expire_access_days']) ? intval($data['sfwd-courses_expire_access_days']) : 0;
+
+    return [
+        'expire_enabled' => $expire_enabled ? true : false,
+        'expire_days' => intval($expire_days),
+        'start_date' => $start_timestamp,
+        'end_date' => $end_timestamp,
+    ];
+}
 
 /********DRM-X 4.0 functions********/
 function getIP(){
@@ -174,24 +172,24 @@ function checkUserExists($client, $username) {
 function addNewUser($client, $username, $userEmail){
 	$add_user_param = array(
 		'AdminEmail' 		=> DRMX_ACCOUNT,
-		'WebServiceAuthStr' => DRMX_AUTHENTICATION,
-		'GroupID' 			=> DRMX_GROUPID,
+		'WebServiceAuthStr' 	=> DRMX_AUTHENTICATION,
+		'GroupID' 		=> DRMX_GROUPID,
 		'UserLoginName' 	=> $username,
 		'UserPassword' 		=> 'N/A',
 		'UserEmail' 		=> $userEmail,
 		'UserFullName' 		=> 'N/A',
-		'Title' 			=> 'N/A',
-		'Company' 			=> 'N/A',
-		'Address' 			=> 'N/A',
-		'City' 				=> 'N/A',
-		'Province' 			=> 'N/A',
-		'ZipCode' 			=> 'N/A',
-		'Phone' 			=> 'N/A',
+		'Title' 		=> 'N/A',
+		'Company' 		=> 'N/A',
+		'Address' 		=> 'N/A',
+		'City' 			=> 'N/A',
+		'Province' 		=> 'N/A',
+		'ZipCode' 		=> 'N/A',
+		'Phone' 		=> 'N/A',
 		'CompanyURL' 		=> 'N/A',
 		'SecurityQuestion' 	=> 'N/A',
 		'SecurityAnswer' 	=> 'N/A',
-		'IP' 				=> getIP(),
-		'Money' 			=> '0',
+		'IP' 			=> getIP(),
+		'Money' 		=> '0',
 		'BindNumber' 		=> DRMX_BINDING,
 		'IsApproved' 		=> 'yes',
 		'IsLockedOut' 		=> 'no',
@@ -202,33 +200,31 @@ function addNewUser($client, $username, $userEmail){
 }
 
 function updateRight($client, $lp_duration, $userEmail){
-	$beginDate = date("Y/m/d", strtotime("-2 days"));
-	$ExpirationDate = date("Y/m/d", strtotime("+1 year"));
 
 	$updateRight_param = array(
-		'AdminEmail'				=> DRMX_ACCOUNT,
-		'WebServiceAuthStr'			=> DRMX_AUTHENTICATION,
-		'RightsID'					=> DRMX_RIGHTSID,
-		'Description' 				=> "Courses Rights (Please don't delete)",
-		'PlayCount' 				=> "-1",
-		'BeginDate' 				=> $beginDate,
-		'ExpirationDate' 			=> $ExpirationDate,
-		'ExpirationAfterFirstUse'	=> $lp_duration,
-		'RightsPrice' 				=> "0",
-		'AllowPrint' 				=> "False",
-		'AllowClipBoard' 			=> "False",
-		'AllowDoc'					=> "False",
-		'EnableWatermark' 			=> "True",
-		'WatermarkText' 			=> $userEmail." ++username",
-		'WatermarkArea' 			=> "1,2,3,4,5,",
-		'RandomChangeArea' 			=> "True",
-		'RandomFrquency' 			=> "12",
-		'EnableBlacklist' 			=> "True",
-		'EnableWhitelist' 			=> "True",
-		'ExpireTimeUnit' 			=> "Day",
-		'PreviewTime' 				=> 3,
-		'PreviewTimeUnit' 			=> "Day",
-		'PreviewPage' 				=> 3,
+		'AdminEmail'			=> DRMX_ACCOUNT,
+		'WebServiceAuthStr'		=> DRMX_AUTHENTICATION,
+		'RightsID'			=> DRMX_RIGHTSID,
+		'Description' 			=> "Courses Rights (Please don't delete)",
+		'PlayCount' 			=> "-1",
+		'BeginDate' 			=> $lp_duration['BeginDate'],
+		'ExpirationDate' 		=> $lp_duration['ExpirationDate'],
+		'ExpirationAfterFirstUse'	=> $lp_duration['ExpirationAfterFirstUse'],
+		'RightsPrice' 			=> "0",
+		'AllowPrint' 			=> "False",
+		'AllowClipBoard' 		=> "False",
+		'AllowDoc'			=> "False",
+		'EnableWatermark' 		=> "True",
+		'WatermarkText' 		=> $userEmail." ++username",
+		'WatermarkArea' 		=> "1,2,3,4,5,",
+		'RandomChangeArea' 		=> "True",
+		'RandomFrquency' 		=> "12",
+		'EnableBlacklist' 		=> "True",
+		'EnableWhitelist' 		=> "True",
+		'ExpireTimeUnit' 		=> "Day",
+		'PreviewTime' 			=> 3,
+		'PreviewTimeUnit' 		=> "Day",
+		'PreviewPage' 			=> 3,
 		'DisableVirtualMachine'		=> 'True',
 	);
 	
@@ -249,24 +245,27 @@ function checkUserIsRevoked($client, $username){
 
 
 function getLicense($client, $username){
+	global $drmx_params;
+	
 	$param = array(
 		'AdminEmail'         => DRMX_ACCOUNT,
 		'WebServiceAuthStr'  => DRMX_AUTHENTICATION,
-		'ProfileID'          => $_SESSION["ProfileID"],
-		'ClientInfo'         => $_SESSION["ClientInfo"],
+		'ProfileID'          => $drmx_params["profileid"],
+		'ClientInfo'         => $drmx_params["clientinfo"],
 		'RightsID'           => DRMX_RIGHTSID,
 		'UserLoginName'      => $username,
 		'UserFullName'       => 'N/A',
 		'GroupID'            => DRMX_GROUPID,
 		'Message'            => 'N/A',
 		'IP'                 => getIP(),
-		'Platform'           => $_SESSION["Platform"],
-		'ContentType'        => $_SESSION["ContentType"],
-		'Version'            => $_SESSION["Version"],
+		'Platform'           => $drmx_params["platform"],
+		'ContentType'        => $drmx_params["contenttype"],
+		'Version'            => $drmx_params["version"],
+		'Mac'                => $drmx_params["mac"],
 	);
 	
 	/*****Obtain license by calling getLicenseRemoteToTableWithVersion******/
-	$result = $client->__soapCall('getLicenseRemoteToTableWithVersion', array('parameters' => $param));
+	$result = $client->__soapCall('getLicenseRemoteToTableWithVersionWithMac', array('parameters' => $param));
 	return $result;
 }
 
@@ -283,7 +282,7 @@ function getLicense($client, $username){
 <body>
 	<div class="login-wrap">
        	<div class="login-head">
-          	<div align="center"><img src="public/images/logo.png" alt=""></div>
+	        <div align="center"><img src="public/images/logo.png" alt=""></div>
        	</div>
 		<div class="login-cont">
 			<div id="btl-login-error" class="btl-error">
